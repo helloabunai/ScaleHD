385dde10e3f26136ed8d1b7db00f1312f3e4bd2f
alastairm <alastairm@gmail.com>
2017-05-24 08:08:51 +0100

    exception hotfix (atypical mistake, whoops)

--- src/ScaleHD/sherpa.py
+++ src/ScaleHD/sherpa.py
@@ -262,12 +262,12 @@ class ScaleHD:
 				###############################################
 				## Stage three!! Scan for atypical alleles.. ##
 				###############################################
-				#try:
-				self.atypical_scanning(current_seqpair)
-				#except Exception, e:
-				#	self.append_report(current_seqpair)
-				#	log.info('{}{}{}{}{}: {}\n'.format(clr.red, 'shd__ ', clr.end, 'Atypical scanning failure on ', seqpair_lbl, str(e)))
-				#	continue
+				try:
+					self.atypical_scanning(current_seqpair)
+				except Exception, e:
+					self.append_report(current_seqpair)
+					log.info('{}{}{}{}{}: {}\n'.format(clr.red, 'shd__ ', clr.end, 'Atypical scanning failure on ', seqpair_lbl, str(e)))
+					continue
 
 				##########################################
 				## Stage four!! Process allele status.. ##
