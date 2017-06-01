ed6a6cd6dbb96f5a8e6ca7456dbce1b3de36d3af
alastairm <alastairm@gmail.com>
2017-06-01 09:08:10 +0100

    unix cmd shell calling hotfix

--- src/setup.py
+++ src/setup.py
@@ -13,7 +13,7 @@ setup(
     name='ScaleHD',
 
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.243',
+    version='0.244',
 
     description='Automated DNA micro-satellite genotyping.',
     long_description=long_description,
