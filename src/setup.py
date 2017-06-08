035d72714404d2c42ab5837297cc77eb5fd04041
alastairm <alastairm@gmail.com>
2017-06-08 15:00:46 +0100

    minor distribution scraping hotfix

--- src/setup.py
+++ src/setup.py
@@ -13,7 +13,7 @@ setup(
     name='ScaleHD',
 
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.246',
+    version='0.247',
 
     description='Automated DNA micro-satellite genotyping.',
     long_description=long_description,
