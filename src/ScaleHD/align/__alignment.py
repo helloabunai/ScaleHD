d688824e4a1242070a769c19dc1774c1a2bb1b43
alastairm <alastairm@gmail.com>
2017-06-29 14:55:06 +0100

    stupid awk subsample scope fix

--- src/ScaleHD/align/__alignment.py
+++ src/ScaleHD/align/__alignment.py
@@ -115,21 +115,17 @@ class SeqAlign:
 		awk_process.wait(); awk_output = int(awk_process.communicate()[0])
 		self.sequencepair_object.set_totalseqreads(awk_output)
 		self.sequencepair_object.set_original_fqcount(awk_output)
+
+		self.subsample_flag = 1.0
 		if not self.broad_flag:
 			if awk_output > 100000: self.subsample_flag = 0.15
 			elif 100000 > awk_output > 50000: self.subsample_flag = 0.4
 			elif 50000 > awk_output > 25000: self.subsample_flag = 0.6
 
-		if self.broad_flag:
-			forward_reads = self.sequencepair_object.get_fwreads()
-			reverse_reads = self.sequencepair_object.get_rvreads()
-			self.sequencepair_object.set_fwreads(forward_reads)
-			self.sequencepair_object.set_rvreads(reverse_reads)
-		elif awk_output > 50000:
-			forward_reads = self.subsample_input(self.sequencepair_object.get_fwreads(), 'R1')
-			reverse_reads = self.subsample_input(self.sequencepair_object.get_rvreads(), 'R2')
-			self.sequencepair_object.set_fwreads(forward_reads)
-			self.sequencepair_object.set_rvreads(reverse_reads)
+		forward_reads = self.subsample_input(self.sequencepair_object.get_fwreads(), 'R1')
+		reverse_reads = self.subsample_input(self.sequencepair_object.get_rvreads(), 'R2')
+		self.sequencepair_object.set_fwreads(forward_reads)
+		self.sequencepair_object.set_rvreads(reverse_reads)
 
 		##
 		## Align the two FastQ files in the pair
