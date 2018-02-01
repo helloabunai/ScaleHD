66e70a287a284e6e5f9f109bd77f376e27eadb2f
alastairm <alastairm@gmail.com>
2018-02-01 12:10:46 +0000

    Minor fixes, updates for versioning, init documentation

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -82,7 +82,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -160,8 +160,8 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     (master_doc, 'ScaleHD', u'ScaleHD Documentation',
-     author, 'ScaleHD', 'One line description of project.',
-     'Miscellaneous'),
+     author, 'ScaleHD', 'Automated Huntington Disease genotyping pipeline.',
+     ''),
 ]
 
 
